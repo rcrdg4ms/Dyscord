71f0fdc966c182f62bc2e8f4983322aca73676fb
rcrdg4ms <rigames2004@gmail.com>
2023-11-22 19:34:56 -0300

    First app preview

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-...
+from kivy.app import App
+from kivy.core.window import Window
+
+from interfaces import *
+
+Window.borderless = True
+
+class MainApp(App):
+    def build(self):
+        return Interface()
+
+
+MainApp().run()
