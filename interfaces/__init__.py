f9085ced9af12f9a8dce6dac325657de3e93ed7c
rcrdg4ms <rigames2004@gmail.com>
2024-02-10 16:29:37 -0300

    Group_popup added

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,7 +1,13 @@
 from kivy.uix.floatlayout import FloatLayout
 from interfaces.windowborder import Windowborder
-from interfaces.groups import Group
+from interfaces.groups import Group, Group_popup
+from interfaces.channels import Channels
 
 
 class Interface(FloatLayout):
-    ...
+    def reload_channels(self):
+        self.ids.mainfloat.remove_widget(self.ids.channels)
+        channels = Channels()
+        channels.id = 'channels'
+        channels.width = self.width-72
+        self.ids.mainfloat.add_widget(channels)
--- interfaces/groups.py
+++ interfaces/groups.py
@@ -2,12 +2,13 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivymd.uix.behaviors import HoverBehavior
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.animation import Animation
 from kivy.properties import NumericProperty
 from kivy.effects.scroll import ScrollEffect
-from mouse import Cursor
-import json
+from mouse import Cursor, Window
+from database import localdb, db
 
 
 class Groups_scroll(ScrollView):
@@ -15,111 +16,153 @@ class Groups_scroll(ScrollView):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        usergroups = db.get()[localdb.get()['user']]['groups']
-        floatl = Floatl()
+        usergroups = db.get(localdb.get('user'))['groups']
+        boxl = Boxlg()
 
-        floatl.size_hint_y = None
-        floatl.height = (48+8)*(len(usergroups)+1)+10
-        
-        direct = Directmessages(len(usergroups))
-        direct.position_y = direct.position_y+10
-        floatl.add_widget(direct)
+        boxl.add_widget(Directmessages())
+        boxl.height = (48+8)
 
-        num = len(usergroups)-1
+        widget = Widget()
+        widget.size_hint_y = None
+        widget.height = 10
+        boxl.add_widget(widget)
+        boxl.height += 10
+
         for groupid in usergroups:
-            floatl.add_widget(Group(num, groupid))
-            num -= 1 
+            boxl.add_widget(Group(groupid))
+            boxl.height += 48+8
 
-        self.add_widget(floatl)
-
-    def on_scroll_start(self, touch, check_children=True):
-        if self.parent.inscrollarea:
-            return super().on_scroll_start(touch, check_children)
+        self.add_widget(boxl)
         
     
     def on_touch_move(self, touch):
         ...
 
+    def on_scroll_start(self, touch, check_children=True):
+        return super().on_scroll_start(touch, check_children)
 
-class Floatl(FloatLayout):
-    ...
 
+class Boxlg(BoxLayout):
+    size_hint_y = None
+    orientation = 'vertical'
 
-class _localdb:
-    def get(self):
-        return json.load(open('local.json', 'r'))
+    def change_group(self, groupid):
+        if localdb.get()['group'] == groupid:
+                return
+        for children in self.children:
+            if type(children) != Widget:
+                if children.groupid == localdb.get()['group']:
+                    Animation(
+                        pingsize = 1,
 
-    def change(self, key, value):
-        db = self.get()
-        db[key] = value
-        json.dump(db, open('local.json', 'w'), indent=4)
+                        duration = .1
+                    ).start(children)
 
-class _db:
-    def get(self):
-        return json.load(open('Database.json', 'r'))
+                elif children.groupid == groupid:
+                    Animation(
+                        pingsize = 42,
+
+                        duration = .1
+                    ).start(children)
+        localdb.change('group', groupid)
+        #Window.children[0].reload_channels()
+
+class Group_popup(ScrollView):
+    def label_on(self, parent):
+        self.size_hint_x = 1
+        self.pos_y = parent.pos[1]
+        self.groupname = parent.groupname
+
+        Animation(
+            alpha = 1,
+            
+            d = .1
+        ).start(self)
     
-db = _db()
-localdb = _localdb()
+
+    def label_off(self):
+        self.size_hint_x = None
+        self.width = 0
+
+        Animation(
+            alpha = 0,
+            
+            d = .1
+        ).start(self)
+
+
+    def on_scroll_start(self, touch, check_children=True):
+        ...
 
 
 class Group_button(Button, HoverBehavior):
     def on_enter(self):
+        Window.children[0].ids.grouppopup.label_on(self.parent)
+
         Animation(
             radius = 45/4+45/8,
             color_on = self.parent.colors[1],
             
             d = .1
         ).start(self)
+
         Cursor.add('hand')
 
         self.parent.enter()
     
     def on_leave(self):
+        Window.children[0].ids.grouppopup.label_off()
+
         Animation(
             radius = 45/2,
             color_on = self.parent.colors[0],
 
             d = .1
         ).start(self)
+
         Cursor.remove()
 
         self.parent.leave()
 
     
     def on_press(self):
-        self.pos = (self.pos[0], self.parent.position_y-1)
+        self.pos_y = self.pos[1]
+        self.pos = (self.pos[0], self.pos[1]-1)
     
     def on_release(self):
-        self.pos = (self.pos[0], self.parent.position_y)
-        localdb.change('group', self.parent.groupid)
+        self.pos = (self.pos[0], self.pos_y)
+        self.parent.parent.change_group(self.parent.groupid)
 
 
-class Group(FloatLayout):
-    def __init__(self, num, groupid = None, **kwargs):
+class Group(Widget):
+    def __init__(self, groupid = None, **kwargs):
         super().__init__(**kwargs)
-        self.position_y = ((48+8)*num)+4
 
         self.groupid = groupid
+        if self.groupid == localdb.get('group'):
+            self.pingsize = 42
 
         if groupid:
-            source = db.get()[groupid]['source']
+            source = db.get(groupid)['source']
             if source:
                 self.source = source
 
+    @property
+    def groupname(self):
+        if self.groupid != None:
+            return db.get(self.groupid)['name']
+        return self._groupname
+
     def enter(self):
-        if self.groupid:
-            self.groupname = db.get()[self.groupid]['name']
         Animation(
-            alpha = 1,
-            pingsize = 20,
+            pingsize = 20 if self.groupid != localdb.get()['group'] else self.pingsize,
 
             duration = .1
         ).start(self)
     
     def leave(self):
-                Animation(
-            alpha = 0,
-            pingsize = 1,
+        Animation(
+            pingsize = 1 if self.groupid != localdb.get()['group'] else self.pingsize,
 
             duration = .1
         ).start(self)
